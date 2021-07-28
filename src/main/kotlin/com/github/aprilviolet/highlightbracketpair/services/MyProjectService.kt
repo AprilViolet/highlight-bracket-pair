6a2a4a7169288727e5229969a78ff700e8ce3687
GitHub Action <action@github.com>
2021-07-28 16:03:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/aprilviolet/highlightbracketpair/services/MyProjectService.kt
+++ src/main/kotlin/com/github/aprilviolet/highlightbracketpair/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.aprilviolet.highlightbracketpair.services
 
+import com.github.aprilviolet.highlightbracketpair.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
