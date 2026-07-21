97efe1f927b160e4a8a61ab6f5c3162bbd63f937
AprilViolet <ltao.main@outlook.com>
2026-07-21 23:52:39 +0800

    :construction_worker: refactor(build): upgrade template to v2.6.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,28 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
+import org.jetbrains.intellij.platform.gradle.extensions.intellijPlatform
 
 rootProject.name = "highlight-bracket-pair"
+
+pluginManagement {
+    plugins {
+        id("org.jetbrains.kotlin.jvm") version "2.1.20"
+        id("org.jetbrains.changelog") version "2.5.0"
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("org.jetbrains.intellij.platform.settings") version "2.16.0"
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    // Configure all projects' repositories
+    repositories {
+        mavenCentral()
+
+        // IntelliJ Platform Gradle Plugin Repositories Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-repositories-extension.html
+        intellijPlatform {
+            defaultRepositories()
+        }
+    }
+}
